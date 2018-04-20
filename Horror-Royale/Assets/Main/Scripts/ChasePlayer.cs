c67958a321b7b0030b21a7d882ff05f22a5edfc1
Ryan Zeng <zeng.ryan2@gmail.com>
2018-04-20 18:46:52 -0400

    Added Monsters

---
+++ Horror-Royale/Assets/Main/Scripts/ChasePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasePlayer : MonoBehaviour {
+
+
+	public Transform player;
+	public int distance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(player.position, this.transform.position) < distance){
+			Vector3 direction = player.position - this.transform.position;
+			direction.y -= 1;
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation,
+				Quaternion.LookRotation (direction), 0.1f);
+		}
+	}
+}
