0018577d19f4f4ea1dcbd2726718faee5b19f8f4
Ryan Zeng <zeng.ryan2@gmail.com>
2018-04-22 12:38:01 -0400

    Spooky following monster

---
+++ Horror-Royale/Assets/Main/Scripts/OnCollisionEnter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+
+public class OnCollisionEnter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.name == "Player") {
+			Debug.Log ("MONSTER MONSTER");
+			GameObject.Find ("Player").GetComponent<FirstPersonController> ().enabled = false;
+
+		}
+	
+	}
+
+	IEnumerator ScaryFace(){
+		//GameObject.Find ("JumpImage").Get
+		
+	}
+
+}
