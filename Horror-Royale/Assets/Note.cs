e9c6f578f67afda21750886c7a3d4300ae8dabb1
Harsh Patel <harshpatel7.list@gmail.com>
2018-05-04 19:34:38 -0400

    note and menu

---
+++ Horror-Royale/Assets/Note.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Note : MonoBehaviour 
+{
+	public Image noteImage; 
+
+	public AudioClip pickupSound; 
+
+	public AudioClip putAwaySound; 
+
+	// Use this for initialization
+	void Start () {
+
+		noteImage.enabled = false; 
+	}
+	
+	public void showNoteImage()
+	{
+		noteImage.enabled = true; 
+		GetComponent<AudioSource> ().PlayOneShot (pickupSound);
+	}
+
+	public void hideNoteImage()
+	{
+		noteImage.enabled = false; 
+		GetComponent<AudioSource> ().PlayOneShot (putAwaySound); 
+	}
+}
