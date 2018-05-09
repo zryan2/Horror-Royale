72df8b5046ca33e47b02d4871dffc03baca3942c
Ryan Zeng <zeng.ryan2@gmail.com>
2018-05-09 00:38:48 -0400

    Added MAin Menu stuff

---
+++ Horror-Royale/Assets/Destroy.cs
@@ -0,0 +1,15 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public GameObject destroyedVersion;
+
+	void OnMouseDown()
+	{
+		Instantiate(destroyedVersion, transform.position, transform.rotation);
+		Destroy (gameObject); 
+		
+	}
+}
